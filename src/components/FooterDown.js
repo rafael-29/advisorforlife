50ba30c4c71db7cd0419c20fcc76569f2dd8022c
rafael my master <sorafaelbook@gmail.com>
2021-05-21 15:22:33 +0400

    haynako

--- src/components/FooterDown.js
+++ src/components/FooterDown.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
@@ -9,42 +7,25 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://sunfinancialadvisor.com/">
       sunfinancialadvisor 2021
       </Link>
     </Typography>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-  },
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
-  },
-}));
 
 export default function StickyFooter() {
-  const classes = useStyles();
+  
 
-  return (
-    <div className={classes.root}>
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Typography variant="body1"><a className="termspol-a" href="/termscon">Terms</a> and <a className="termspol-a" href="/policy">Policy</a> agreement</Typography>
-          <Copyright />
-        </Container>
-      </footer>
-    </div>
-  );
+return (
+  <div style={{
+  width: '100%'
+  }}>
+  <Container maxWidth="sm">
+    <Typography variant="body1"><a className="termspol-a" href="/termscon">Terms</a> and <a className="termspol-a" href="/policy">Policy</a> agreement</Typography>
+    <Copyright />
+  </Container>
+  </div>
+);
 }
